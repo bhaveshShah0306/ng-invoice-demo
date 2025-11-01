b5586b0816e1e5082dae347c70a6e3ad06185649
bShah <bhaveshShah0306@users.noreply.github.com>
2025-11-01 07:33:13 +0530

    banking app changes : added components,services,models

---
+++ src/app/banking/components/transaction-panel/transaction-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-panel.component.html',
+  styleUrl: './transaction-panel.component.css'
+})
+export class TransactionPanelComponent {
+
+}
