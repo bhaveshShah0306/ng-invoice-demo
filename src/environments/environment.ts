6aec6d403a61d7c20c0e88b090e2f41661281a05
bShah <bhaveshShah0306@users.noreply.github.com>
2025-11-01 08:39:56 +0530

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,9 @@ export const environment = {
     APIUrl:'http://localhost:3000/product',
     displayname:'Angular 18 App in Production'
 };
+// export const environment = {
+//     production: true,
+//     APIUrl: 'http://localhost:3000/product',
+//     BankingAPIUrl: 'https://localhost:7143', // Your .NET 8 API URL
+//     displayname: 'Angular 18 Banking App - Production'
+// };
